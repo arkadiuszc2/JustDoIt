5489b208a8456b12a97645ef687455c887a244cc
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-01-17 22:58:28 +0100

    add data seeder to backend

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO category (id, name, description) VALUES (10, 'Sport', 'Tasks connected with sport');
+INSERT INTO task (id, title, description, priority, status, category) VALUES (10, 'Create training', 'Create bag workout and cross fit workout', 'HIGH', 'TO_DO', 10);
