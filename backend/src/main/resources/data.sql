a144299534f61f1af5ec348f6f17337c4f93f5f8
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-01-17 23:32:42 +0100

    Fix data seeder and add more data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,6 @@
-INSERT INTO category (id, name, description) VALUES (10, 'Sport', 'Tasks connected with sport');
-INSERT INTO task (id, title, description, priority, status, category) VALUES (10, 'Create training', 'Create bag workout and cross fit workout', 'HIGH', 'TO_DO', 10);
+INSERT INTO category (id, name, description) VALUES (1, 'Sport', 'Tasks connected with sport');
+INSERT INTO task (id, title, description, priority, status, category_id) VALUES (1, 'Create training', 'Create bag workout and cross fit workout', 'HIGH', 'TODO', 1);
+INSERT INTO category (id, name, description) VALUES (2, 'Studies', 'Studies, homework and my own projects');
+INSERT INTO task (id, title, description, priority, status, category_id) VALUES (2, 'Add feature to project', 'Create endpoint /api/posts/{id} in SpeakApp', 'MEDIUM', 'DONE', 2);
+INSERT INTO category (id, name, description) VALUES (3, 'Friends', 'Meetings, parties and all about friends');
+INSERT INTO task (id, title, description, priority, status, category_id) VALUES (3, 'Buy present', 'Buy big jagermeister in Lidl for Krzysio', 'LOW', 'TODO', 3);
