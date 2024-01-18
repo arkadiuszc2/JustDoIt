d60f594a7e49ae3e4fbaef9a7010b5a1386e9da8
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-01-18 16:46:52 +0100

    Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import Navbar from './components/Navbar';
 import Tasks from './components/Tasks';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-      <Tasks />
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Tasks />} />
+          </Routes>
+        </div>
       </div>
-    </div>
   );
 }
 
