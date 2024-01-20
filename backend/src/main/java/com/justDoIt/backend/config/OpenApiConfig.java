d43c2b81db3a9c8537da8e6decaab83fdbe19d26
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2024-01-21 00:03:41 +0100

    Configure swagger to include jwt header

--- backend/src/main/java/com/justDoIt/backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/justDoIt/backend/config/OpenApiConfig.java
@@ -1,16 +1,31 @@
 package com.justDoIt.backend.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 
-@OpenAPIDefinition(
-    info = @Info(
-        title = "Backend API",
-        contact = @Contact(name = "Arkadiusz Ciepliński", email = "arkadiusz.cieplinski2@gmail.com"),
-        version = "1.0"
-    )
-)
 public class OpenApiConfig {
-
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Backend API")
+                .version("1.0")
+                .contact(new Contact()
+                    .name("Arkadiusz")
+                    .email("arkadiusz@gmail.com"))
+                .license(new License()
+                    .name("License of API")
+                    .url("API license URL")))
+            .components(new Components()
+                .addSecuritySchemes("bearer-key",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
 }
